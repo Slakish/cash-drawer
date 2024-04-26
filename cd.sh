c1c20c79ff8cadbfe9ef18bc86bfb7023e368991
Slakish <36281972+Slakish@users.noreply.github.com>
2024-04-26 14:18:56 +0200

    Add files via upload

---
+++ cd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Schritt 1: Ins Verzeichnis wechseln
+cd /usr/local/sbin
+
+# Schritt 2: Funktionen herunterladen
+wget -q http://sven-ola.commando.de/functions.sh
+
+# Schritt 3: Skript herunterladen
+wget -q http://sven-ola.commando.de/013-cashdrawer.sh
+
+# Schritt 4: Berechtigungen setzen
+chmod +x *.sh
+
+# Schritt 5: Skript mit manuellem Modus ausführen und Enter drücken
+/usr/local/sbin/013-cashdrawer.sh manual <<< $'\n'
+
+# Warte kurz, um sicherzustellen, dass das Skript geladen wurde
+sleep 1
+
+# Schritt 6: Automatisch 'o' (für Öffnen) drücken
+echo -n "o" | /usr/local/sbin/013-cashdrawer.sh manual
+
+# Schritt 7: Beenden des Skripts
+echo "Das Skript wurde automatisch ausgeführt."
